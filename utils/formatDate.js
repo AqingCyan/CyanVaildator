d898623e005266c8f4ef1b85d22b89f9fc5bfa38
AqingCyan <aqingcyan@cyanthing.com>
2019-05-31 00:47:43 +0800

    feat(utils): 工具函数

---
+++ utils/formatDate.js
@@ -0,0 +1,28 @@
+const dayjs = require('dayjs')
+const defaultOptions = {
+  formatOption: 'YYYY-MM-DD',
+  getYear: false,
+  getMonth: false,
+  getDay: false
+}
+/**
+ * 格式化日期
+ * 
+ * @param {string} date 
+ * @param {object} config 
+ */
+function formatDate(date = (new Date), config = {}) {
+  const conf = Object.assign({}, defaultOptions, config)
+  const Data = {}
+  Data.formatResult = dayjs(date).format(conf.formatOption)
+  if (conf.getYear) {
+    Data.year = dayjs(date).year()
+  }
+  if (conf.getMonth) {
+    Data.month = dayjs(date).month()
+  }
+  if (conf.getDay) {
+    Data.day = dayjs(date).day()
+  }
+  return Data
+}
