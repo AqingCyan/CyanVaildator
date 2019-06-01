d252575e4c41875a468232994cc1ae1c3d72f1a0
AqingCyan <aqingcyan@cyanthing.com>
2019-06-01 18:35:39 +0800

    core(package): 配置文件修改

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import commonjs from 'rollup-plugin-commonjs'
 import resolve from 'rollup-plugin-node-resolve'
-import babel from 'rollup-plugin-babel';
 
 export default {
   input: 'index.js',
@@ -10,9 +9,6 @@ export default {
   },
   plugins: [
     resolve(),
-    babel({
-      exclude: 'node_modules/**' // 只编译我们的源代码
-    }),
     commonjs()
   ]
 }
